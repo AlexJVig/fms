cc158050cff645bb1486984aa18b35f22c9b555e
Alex <alzombii@gmail.com>
2022-09-08 13:57:23 +0300

    Added basic classes

---
+++ fms/Index.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace fmsServer
+{
+    public class Index
+    {
+        public int Id { get; set; }
+        public double LatestQuote { get; set; }
+        public List<Stock> Stocks { get; set; }
+
+        public Index(int id, double latestQuote, List<Stock> stocks)
+        {
+            Id = id;
+            LatestQuote = latestQuote;
+            Stocks = stocks;
+        }
+    }
+}
---
+++ fms/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace fmsServer
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public decimal LatestQuote { get; set; }
+
+        public Stock(int id, int latestQuote)
+        {
+            Id = id;
+            LatestQuote = latestQuote;
+        }
+    }
+}
